c3f51684f99397e996ae54cb9b4e2557407a7bb0
haoyangyang <1622072353@qq.com>
2021-04-04 21:08:56 +0800

    feat(product): 添加生产环境代码

--- src/common/element.ts
+++
@@ -1,8 +0,0 @@
-import ElementPlus from 'element-plus'
-import locale from 'element-plus/lib/locale/lang/zh-cn'
-import 'dayjs/locale/zh-cn'
-import 'element-plus/lib/theme-chalk/index.css';
-
-export default function initElement(App: any) {
-    App.use(ElementPlus, { locale })
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import initElement from '@/common/element';
-import App from './App.vue'
-import router from './router/index';
-
-
-let app = createApp(App)
-initElement(app);
-app.use(router).mount('#app');
